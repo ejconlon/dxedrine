f79de4edc2292c3849db72c442b500aea7a8d35b
Eric Conlon <ejconlon@gmail.com>
2016-06-15 22:30:58 -0700

    get

--- src/Dxedrine.hs
+++ src/Dxedrine.hs
@@ -1,9 +1,63 @@
-module Dxedrine
-    ( someFunc
-    ) where
+module Dxedrine where
 
+import Control.Monad (unless)
 import Data.Binary
-import Data.ByteString hiding (putStrLn)
+import Data.Binary.Get
+import Data.Bits
+import qualified Data.ByteString.Lazy as BL
+import Data.Word
+
+data DxParamChange = DxParamChange
+  { _dpcManfId :: Word8
+  , _dpcDevice :: Word8
+  , _dpcParamGroup :: Word8
+  , _dpcParam :: Word8
+  , _dpcData :: Word8
+  } deriving (Show, Eq)
+
+sysexStart :: Word8
+sysexStart = 0xF0
+
+sysexEnd :: Word8
+sysexEnd = 0xF7
+
+yamahaMfrId :: Word8
+yamahaMfrId = 0x43
+
+system1ModelId :: Word8
+system1ModelId = 0x62
+
+system2ModelId :: Word8
+system2ModelId = 0x6D
+
+instance Binary DxParamChange where
+  get = do
+    start <- getWord8
+    unless (start == sysexStart) $ fail "must start with sysex start, 0xF0"
+    manfId <- getWord8
+    deviceRaw <- getWord8
+    unless (deviceRaw .&. 0x10 == 0x10) $ fail "device fails 0x10 test"
+    paramGroup <- getWord8
+    param <- getWord8
+    dataa <- getWord8
+    end <- getWord8
+    unless (end == sysexEnd) $ fail "must end with sysex end, 0xF7"
+    return DxParamChange
+      { _dpcManfId = manfId
+      , _dpcDevice = deviceRaw .&. 0x0F
+      , _dpcParamGroup = paramGroup
+      , _dpcParam = param
+      , _dpcData = dataa
+      }
+
+  put dpc = do
+    putWord8 sysexStart
+    putWord8 $ _dpcManfId dpc
+    putWord8 $ ((_dpcDevice dpc) .|. 0x10)
+    putWord8 $ _dpcParamGroup dpc
+    putWord8 $ _dpcParam dpc
+    putWord8 $ _dpcData dpc
+    putWord8 sysexEnd
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,30 +1,52 @@
+import Data.Binary
+import Data.Binary.Get
+import qualified Data.ByteString.Lazy as BL
+import Data.Word
 import Dxedrine
 import Test.Tasty
 import Test.Tasty.HUnit
 
-dxParamChangeBytes :: [Word]
+dxParamChangeBytes :: [Word8]
 dxParamChangeBytes = [0xF0, 0x43, 0x10, 0x19, 0x4D, 0x00, 0xF7]
 
 data Msg = Msg
 
-dxParamChangeMsg :: Msg
-dxParamChangeMsg = Msg
+dxParamChangeMsg :: DxParamChange
+dxParamChangeMsg = DxParamChange
+  { _dpcManfId = 0x43
+  , _dpcDevice = 0x00
+  , _dpcParamGroup = 0x19
+  , _dpcParam = 0x4D
+  , _dpcData = 0x00
+  }
 
-dx200NativeBulkDumpBytes :: [Word]
-dx200NativeBulkDumpBytes = [0xF0, 0x43, 0x00, 0x62, 0x00, 0x05, 0x21, 0x7F,
-                            0x00, 0x03, 0x00, 0x01, 0x0C, 0x32, 0x19, 0xF7]
+--dx200NativeBulkDumpBytes :: [Word8]
+--dx200NativeBulkDumpBytes = [0xF0, 0x43, 0x00, 0x62, 0x00, 0x05, 0x21, 0x7F,
+--                            0x00, 0x03, 0x00, 0x01, 0x0C, 0x32, 0x19, 0xF7]
 
-dx200NativeBulkDumpMsg :: Msg
-dx200NativeBulkDumpMsg = Msg
+--dx200NativeBulkDumpMsg :: Msg
+--dx200NativeBulkDumpMsg = Msg
 
-parses :: String -> [Word] -> Msg -> TestTree
-parses name bytes msg = testCase name $ do
-  return ()
+runGetOrError :: Get a -> BL.ByteString -> Either String a
+runGetOrError g bs =
+  case runGetOrFail g bs of
+    Left (_, _, s) -> Left s
+    Right (_, _, a) -> Right a
+
+-- TODO change to MonadFail constraint
+unwrap :: Monad m => Either String a -> m a
+unwrap (Left s) = fail s
+unwrap (Right a) = return a
+
+decodes :: (Binary a, Eq a, Show a) => String -> [Word8] -> a -> TestTree
+decodes name bytes msg = testCase name $ do
+  decoded <- unwrap $ runGetOrError get $ BL.pack bytes
+  decoded @?= msg
 
 tests :: TestTree
 tests = testGroup "Tests" [
-    parses "dx param change" dxParamChangeBytes dxParamChangeMsg
-  , parses "dx200 native bulk dump" dx200NativeBulkDumpBytes dx200NativeBulkDumpMsg
+    decodes "dx param change" dxParamChangeBytes dxParamChangeMsg
+  --, parses "dx200 native bulk dump" dx200NativeBulkDumpBytes dx200NativeBulkDumpMsg
   ]
 
 main :: IO ()
