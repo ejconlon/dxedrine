3d1c9b90e9a7f229e5ff06927fb49748a4f5851b
Eric Conlon <ejconlon@gmail.com>
2016-06-15 23:18:44 -0700

    chk left

--- src/Dxedrine.hs
+++ src/Dxedrine.hs
@@ -1,8 +1,9 @@
 module Dxedrine where
 
-import Control.Monad (unless)
+import Control.Monad (forM_, unless)
 import Data.Binary
 import Data.Binary.Get
+import Data.Binary.Put
 import Data.Bits
 import qualified Data.ByteString.Lazy as BL
 import Data.Word
@@ -44,7 +45,7 @@ instance Binary DxParamChange where
     unless (start == sysexStart) $ fail "must start with sysex start, 0xF0"
     manfId <- getWord8
     deviceRaw <- getWord8
-    unless (deviceRaw .&. 0x10 == 0x10) $ fail "device fails 0x10 test"
+    unless (deviceRaw .&. 0xF0 == 0x10) $ fail "device fails 0xF0 test"
     paramGroup <- getWord8
     param <- getWord8
     dataa <- getWord8
@@ -67,12 +68,56 @@ instance Binary DxParamChange where
     putWord8 $ _dpcData dpc
     putWord8 sysexEnd
 
+getN :: Get a -> Integer -> Get [a]
+getN _ 0 = return []
+getN g i = do
+  x <- g
+  xs <- getN g (i - 1)
+  return $ x : xs
+
+makeD2bdChecksum :: Dx200BulkDump -> Word8
+makeD2bdChecksum d2bd = 0
+
 instance Binary Dx200BulkDump where
   get = do
-    undefined
+    start <- getWord8
+    unless (start == sysexStart) $ fail "must start with sysex start, 0xF0"
+    manfId <- getWord8
+    deviceRaw <- getWord8
+    unless (deviceRaw .&. 0xF0 == 0x00) $ fail "device fails 0xF0 test"
+    modelId <- getWord8
+    count <- getWord16be
+    addrHigh <- getWord8
+    addrMid <- getWord8
+    addrLow <- getWord8
+    dataa <- getN getWord8 $ toInteger count
+    checksum <- getWord8
+    -- check checksum
+    end <- getWord8
+    unless (end == sysexEnd) $ fail "must end with sysex end, 0xF7"
+    return Dx200BulkDump
+      { _d2bdManfId = manfId
+      , _d2bdDevice = deviceRaw
+      , _d2bdModelId = modelId
+      , _d2bdAddr = (addrHigh, addrMid, addrLow)
+      , _d2bdData = dataa
+      }
 
-  put d2nbd = do
-    undefined
+  put d2bd = do
+    putWord8 sysexStart
+    putWord8 $ _d2bdManfId d2bd
+    putWord8 $ _d2bdDevice d2bd
+    putWord8 $ _d2bdModelId d2bd
+    let dataa = _d2bdData d2bd
+    let count = (fromIntegral (length dataa)) :: Word16
+    putWord16be count
+    let (addrHigh, addrMid, addrLow) = _d2bdAddr d2bd
+    putWord8 addrHigh
+    putWord8 addrMid
+    putWord8 addrLow
+    forM_ dataa putWord8
+    putWord8 $ makeD2bdChecksum d2bd
+    putWord8 sysexEnd
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
