8f196ddc654877954aa2fbeeb8954028081f2ee0
Eric Conlon <ejconlon@gmail.com>
2016-06-18 16:15:38 -0700

    processing

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,16 @@
 module Main where
 
+import Control.Monad (forM_, unless)
 import Dxedrine
 
 main :: IO ()
 main = do
   putStrLn "parsing"
   e <- parseDxUnions
-  putStrLn $ show $ keepFailed e
-  --putStrLn $ show e
+  let numFailed = length $ keepFailed e
+  unless (numFailed == 0) $ fail $ "number of failed messages: " ++ show numFailed
+  let unions = keepSuccessful e
+  forM_ unions $ \x -> do
+    putStrLn $ show x
   putStrLn "done"
   return ()
