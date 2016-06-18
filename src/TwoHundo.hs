bb4aeaacb1baf1f5556b1994b4772c0e9e099948
Eric Conlon <ejconlon@gmail.com>
2016-06-18 12:23:11 -0700

    stuff

--- src/TwoHundo.hs
+++ src/TwoHundo.hs
@@ -2,3 +2,22 @@ module TwoHundo where
 
 import Dxedrine
 
+data AnnoType =
+    Pattern
+  | Part
+  | Song
+  | Measure
+  deriving (Show, Eq)
+
+data DataType =
+    VoiceCommon1
+  | VoiceCommon2
+  | VoiceScene
+  | VoiceFreeEg
+  | VoiceStepSeq
+  | Effect
+  | PartMix
+  | RhythmStepSeq
+  | SongData
+  deriving (Show, Eq)
+
